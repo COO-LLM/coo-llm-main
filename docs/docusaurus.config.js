f458895129bb6b4f05097b1a1c9d6d81cc9f62eb
khanh2906 <khanhphungdai@gmail.com>
2025-10-13 14:14:19 +0700

    update docs config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,12 +11,12 @@ const config = {
   url: 'https://coo-llm.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/coo-llm-main/',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
    organizationName: 'coo-llm', // Usually your GitHub org/user name.
-   projectName: 'coo-llm-main', // Usually your repo name.
+   projectName: 'docs', // Usually your repo name.
 
   onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
